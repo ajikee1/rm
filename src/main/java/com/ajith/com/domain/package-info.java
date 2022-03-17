e8bbbdbf37489698527c6d7f1182ecf4880d2682
Ajith Keerikkattil <ajithkeerikkattil@ajiths-mbp.myfiosgateway.com>
2022-03-17 11:42:37 -0400

    Initial version of releaseManagement generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/ajith/com/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.ajith.com.domain;
---
+++ src/main/java/com/ajith/com/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ajith.com.repository;
+
+import com.ajith.com.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
